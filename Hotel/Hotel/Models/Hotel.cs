f02be1cbb6ae862ec182ae9cbeda25b76f554b8e
chandanm3 <chandan.mishra@auquall.com>
2019-03-11 15:18:56 +0530

    project added

---
+++ Hotel/Hotel/Models/Hotel.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Hotel.Models
+{
+    public class Hotelm
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public long Mobile { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ Hotel/Hotel/Models/HotelContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Hotel.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hotel.Models
+{
+    public class HotelContext:DbContext
+    {
+
+        public HotelContext(Microsoft.EntityFrameworkCore.DbContextOptions<HotelContext> options)
+           : base(options)
+        {
+        }
+
+        public Microsoft.EntityFrameworkCore.DbSet<Hotelm> Hotels { get; set; }
+    }
+}
